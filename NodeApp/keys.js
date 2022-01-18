b1c9f82abe4a93122101cd3e353c6324230352b1
arunkumarshahi@gmail.com <arunkumarshahi@gmail.com>
2022-01-18 15:59:25 +0530

    updated node yaml to read env props

--- NodeApp/keys.js
+++ NodeApp/keys.js
@@ -1,12 +1,6 @@
 module.exports = {
-  redisHost: process.env.REDIS_HOST,
-  redisPort: process.env.REDIS_PORT,
-  pgUser: process.env.PGUSER,
-  pgHost: process.env.PGHOST,
-  pgDatabase: process.env.PGDATABASE,
-  pgPassword: process.env.PGPASSWORD,
-  port: process.env.PORT,
-  dbUrl: process.env.ME_CONFIG_MONGODB_SERVER, //"127.0.0.1:8080",
-  username: process.env.ME_CONFIG_MONGODB_ADMINUSERNAME, //"dXNlcm5hbWU=", //process.env.ME_CONFIG_MONGODB_ADMINUSERNAME,
-  password: process.env.ME_CONFIG_MONGODB_ADMINPASSWORD, //"cGFzc3dvcmQ=",
+  port: process.env.port,
+  dbUrl: process.env.db_url, //"127.0.0.1:8080",
+  username: process.env["mongo-root-username"], //"dXNlcm5hbWU=", //process.env.ME_CONFIG_MONGODB_ADMINUSERNAME,
+  password: process.env["mongo-root-password"], //"cGFzc3dvcmQ=",
 };
